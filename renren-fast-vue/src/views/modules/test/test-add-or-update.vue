26053b1b9a58406349ae2d513d1715809a95b40b
xpxwo100 <1066923210@qq.com>
2021-06-28 20:52:11 +0800

    提交一波

---
+++ renren-fast-vue/src/views/modules/test/test-add-or-update.vue
@@ -0,0 +1,120 @@
+<template>
+  <el-dialog
+    :title="!dataForm.id ? '新增' : '修改'"
+    :close-on-click-modal="false"
+    :visible.sync="visible">
+    <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()" label-width="80px">
+    <el-form-item label="name" prop="name">
+      <el-input v-model="dataForm.name" placeholder=""></el-input>
+    </el-form-item>
+    <el-form-item label="code" prop="code">
+      <el-input v-model="dataForm.code" placeholder=""></el-input>
+    </el-form-item>
+    <el-form-item label="age" prop="age">
+      <el-input v-model="dataForm.age" placeholder=""></el-input>
+    </el-form-item>
+    <el-form-item label="neirong" prop="neirong">
+      <el-input v-model="dataForm.neirong" placeholder=""></el-input>
+    </el-form-item>
+    <el-form-item label="test2id" prop="test2id">
+      <el-input v-model="dataForm.test2id" placeholder=""></el-input>
+    </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button type="primary" @click="dataFormSubmit()">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        visible: false,
+        dataForm: {
+          id: 0,
+          name: '',
+          code: '',
+          age: '',
+          neirong: '',
+          test2id: ''
+        },
+        dataRule: {
+          name: [
+            { required: true, message: '不能为空', trigger: 'blur' }
+          ],
+          code: [
+            { required: true, message: '不能为空', trigger: 'blur' }
+          ],
+          age: [
+            { required: true, message: '不能为空', trigger: 'blur' }
+          ],
+          neirong: [
+            { required: true, message: '不能为空', trigger: 'blur' }
+          ],
+          test2id: [
+            { required: true, message: '不能为空', trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    methods: {
+      init (id) {
+        this.dataForm.id = id || 0
+        this.visible = true
+        this.$nextTick(() => {
+          this.$refs['dataForm'].resetFields()
+          if (this.dataForm.id) {
+            this.$http({
+              url: this.$http.adornUrl(`/com-niubility-client/test/info/${this.dataForm.id}`),
+              method: 'get',
+              params: this.$http.adornParams()
+            }).then(({data}) => {
+              if (data && data.code === 0) {
+                this.dataForm.name = data.test.name
+                this.dataForm.code = data.test.code
+                this.dataForm.age = data.test.age
+                this.dataForm.neirong = data.test.neirong
+                this.dataForm.test2id = data.test.test2id
+              }
+            })
+          }
+        })
+      },
+      // 表单提交
+      dataFormSubmit () {
+        this.$refs['dataForm'].validate((valid) => {
+          if (valid) {
+            this.$http({
+              url: this.$http.adornUrl(`/com-niubility-client/test/${!this.dataForm.id ? 'save' : 'update'}`),
+              method: 'post',
+              data: this.$http.adornData({
+                'id': this.dataForm.id || undefined,
+                'name': this.dataForm.name,
+                'code': this.dataForm.code,
+                'age': this.dataForm.age,
+                'neirong': this.dataForm.neirong,
+                'test2id': this.dataForm.test2id
+              })
+            }).then(({data}) => {
+              if (data && data.code === 0) {
+                this.$message({
+                  message: '操作成功',
+                  type: 'success',
+                  duration: 1500,
+                  onClose: () => {
+                    this.visible = false
+                    this.$emit('refreshDataList')
+                  }
+                })
+              } else {
+                this.$message.error(data.msg)
+              }
+            })
+          }
+        })
+      }
+    }
+  }
+</script>
