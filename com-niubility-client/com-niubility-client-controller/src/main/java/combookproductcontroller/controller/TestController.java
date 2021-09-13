8840d5d17908b55f91c77e0dddb85982fb1091a4
xpxwo100 <1066923210@qq.com>
2021-09-13 18:55:08 +0800

    提交一波定时

--- com-niubility-client/com-niubility-client-controller/src/main/java/combookproductcontroller/controller/TestController.java
+++ com-niubility-client/com-niubility-client-controller/src/main/java/combookproductcontroller/controller/TestController.java
@@ -3,6 +3,9 @@ package combookproductcontroller.controller;
 import boot.nettyClient.NettyRpcUtil;
 import boot.nettyRpcModel.ObjectDto;
 import boot.util.R;
+import com.aspose.cells.PageSetup;
+import com.aspose.cells.Workbook;
+import com.aspose.cells.Worksheet;
 import combookproductcontroller.dao.TestDao;
 import combookproductcontroller.entity.TestEntity;
 import combookproductcontroller.interfaces.SchedualServiceHi;
@@ -10,6 +13,7 @@ import combookproductcontroller.service.TestService;
 import combookproductcontroller.util.PageUtils;
 import combookproductcontroller.util.RedisUtils;
 import combookproductcontroller.util.RedissonUtil;
+import combookproductcontroller.util.aspose.AsposeUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.junit.jupiter.api.Test;
@@ -164,4 +168,102 @@ public class TestController {
         return R.ok();
     }
 
+
+    protected HashMap<String,Object> printSpecialHandle(AsposeUtil cellsUtil, Cells cells, List<HashMap<String, Object>> detailList, Integer minPrintRow, int minPrintRowIndex, int paginationRowIndex, int paginationColumnIndex, boolean printOtherInfo)throws Exception {
+        HashMap<String,Object> value = new HashMap<>();
+        Workbook workbook = cellsUtil.getWorkBook();
+        List<List<HashMap<String,Object>>> rowLists = new ArrayList<>();//有分页的每页List
+
+        Worksheet w = workbook.getWorksheets().get(0);
+        PageSetup pageSetup = w.getPageSetup();
+        double topMargin = pageSetup.getTopMargin();//上页边距
+        double bottomMargin = pageSetup.getBottomMargin();//下页边距
+        double zoom = pageSetup.getZoom();//页面缩放比例
+        //复制页签1的内容到页签2
+        int totalPage = 0 ;//总共几页
+        boolean isSurplus = false;//是否有余数，不够整数页
+        int size = detailList.size();//子表明细数量
+        int rowIndex = minPrintRowIndex;//记录模板明细行的行号
+        int maxDataRow = cells.getMaxDataRow();
+        double divisor = 0.353357*(zoom/100);
+        double allHeight = 0.0;
+        double a4Height = 297-((topMargin+bottomMargin)*10);//264
+        //1磅等于0.353357mm     A4 (210mm X 297mm)
+        for(int r=0;r<cells.getMaxDataRow()+1;r++){
+            allHeight += cells.getRowHeight(r); //181
+        }
+        double rowIndexHeight = cells.getRowHeight(rowIndex);
+        double rowHeightMM = rowIndexHeight*divisor;//明细行高度mm
+        double allHeightMM = allHeight*divisor;//111.2
+
+        if(size % minPrintRow > 0){//有余数，+1页
+            totalPage = (size/minPrintRow) + 1;
+            isSurplus = true;
+        }else{
+            totalPage = size/minPrintRow;
+        }
+        //只有一页的不需要分页
+        if(totalPage <= 1){
+            setPageAndFont(totalPage, cells, paginationRowIndex, paginationColumnIndex,null,-1);
+            value.put("intervalList", Lists.newArrayList());
+            return value;
+        }
+
+        //根据页数从页签2复制表头表体模板到页签1
+        List<Integer> intervalList = Lists.newArrayList();
+        if(totalPage>1){
+            double allDataHeightMM = (allHeightMM-rowHeightMM)+(minPrintRow*rowHeightMM);
+            double nullMM = a4Height-allDataHeightMM;//138.162587
+
+            int a = (int)Math.round(nullMM/(13.5*divisor));//28
+            for(int k=0;k<totalPage;k++){
+                //把数据根据页数进行分割
+                List<HashMap<String, Object>> mainList = null;
+                if(isSurplus && (k+1) == totalPage){
+                    mainList = detailList.subList(k*minPrintRow, k*minPrintRow + (size % minPrintRow));
+                }else{
+                    mainList = detailList.subList(k*minPrintRow, (k+1)*minPrintRow);
+                }
+                rowLists.add(mainList);
+            }
+            for(int k=0;k<totalPage-1;k++){
+                int interval=maxDataRow+2+a;//16
+                intervalList.add(interval*(k+1));//[16, 32]
+            }
+            setPageAndFont(totalPage, cells, paginationRowIndex, paginationColumnIndex,null,-1);
+            for(int k=0;k<intervalList.size();k++){
+                cells.copyRows(cells, 0, intervalList.get(k), maxDataRow+1);
+
+                setPageAndFont(totalPage, cells, paginationRowIndex, paginationColumnIndex,intervalList,k);
+                //设置每页模板样式
+                for(int r=0;r<cells.getMaxColumn()+1;r++){
+                    int templateRow = 0;
+                    templateRow = intervalList.get(k) + rowIndex;
+                    com.aspose.cells.Cell templateCellRow = cells.get(templateRow,r);
+                    if(templateCellRow.getValue()!=null){
+                        String cellStr = templateCellRow.getValue().toString().replace("&=[mainMap]", "&=[mainMap"+(k+1)+"]");
+                        templateCellRow.setValue(cellStr);
+                    }
+                }
+            }
+            //填充数据
+            //是否打印图片
+            if(!printOtherInfo){
+                if(rowLists.size()>0){
+                    for(int i=0;i<rowLists.size();i++){
+                        HashMap<String,Object> maps = new HashMap<>();
+                        if(i==0){
+                            maps.put("mainMap", rowLists.get(i));
+                        }else{
+                            maps.put("mainMap"+i, rowLists.get(i));
+                        }
+                        cellsUtil.setDataSource(maps);
+                    }
+                }
+            }
+        }
+        value.put("intervalList", intervalList);
+        value.put("rowLists",rowLists);
+        return value;
+    }
 }
