d9e242b5a322602eb7776eef2eb2dcd385d75668
xpxwo100 <1066923210@qq.com>
2022-02-14 22:55:58 +0800

    提交一波定时

--- com-niubility-server/com-niubility-gateway/src/main/java/combookserver/server/GatewayServerApplication.java
+++ com-niubility-server/com-niubility-gateway/src/main/java/combookserver/server/GatewayServerApplication.java
@@ -1,10 +1,16 @@
 package combookserver.server;
 
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.listener.NamingEvent;
+import com.alibaba.nacos.api.naming.pojo.Instance;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import java.util.List;
 
 @EnableFeignClients
 @EnableDiscoveryClient
@@ -12,11 +18,34 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableRabbit
 public class GatewayServerApplication {
 
-    //test
-    //GatewayServerApplication
     public static void main(String[] args) {
         SpringApplication.run(GatewayServerApplication.class, args);
-
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                test();
+            }
+        }).start();
     }
+    public static void test() {
+        String serverAddr = "localhost:8848";
+        String serviceName = "com-niubility-client-controller";
+        try {
+            NamingService namingService = NacosFactory.createNamingService(serverAddr);
 
+            // 注册监听器
+            namingService.subscribe(serviceName, (event) ->
+            {
+                if (event instanceof NamingEvent) {
+                    List<Instance> list = ((NamingEvent) event).getInstances();
+                    for (Instance temp : list) {
+                        System.out.println("========" + temp);
+                    }
+                }
+
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
