9ccc156174a240f42143a9bacb8f42ab067c9f6a
xpxwo100 <1066923210@qq.com>
2022-02-14 23:18:06 +0800

    提交一波定时

--- com-niubility-server/com-niubility-gateway/src/main/java/combookserver/server/config/CorsConfig.java
+++ com-niubility-server/com-niubility-gateway/src/main/java/combookserver/server/config/CorsConfig.java
@@ -8,6 +8,12 @@
 
 package combookserver.server.config;
 
+import com.alibaba.cloud.nacos.NacosConfigProperties;
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.listener.NamingEvent;
+import com.alibaba.nacos.api.naming.pojo.Instance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -15,11 +21,16 @@ import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.util.pattern.PathPatternParser;
 
+import javax.annotation.PostConstruct;
+import java.util.List;
+
 /**
  * 跨域设置
  */
 @Configuration
 public class CorsConfig {
+    @Autowired
+    public  NacosConfigProperties nacosConfigProperties;
     @Bean
     public  CorsWebFilter  CorsWebFilter(){
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
@@ -30,5 +41,29 @@ public class CorsConfig {
         config.setAllowCredentials(true);
         source.registerCorsConfiguration("/**", config);
         return new CorsWebFilter(source);
+    }
+
+
+    @PostConstruct
+    public  void test() {
+        String serverAddr1 = nacosConfigProperties.getServerAddr();
+        String serviceName = "com-niubility-client-controller";
+        try {
+            NamingService namingService = NacosFactory.createNamingService(serverAddr1);
+
+            // 注册监听器
+            namingService.subscribe(serviceName, (event) ->
+            {
+                if (event instanceof NamingEvent) {
+                    List<Instance> list = ((NamingEvent) event).getInstances();
+                    for (Instance temp : list) {
+                        System.out.println("========" + temp);
+                    }
+                }
+
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
